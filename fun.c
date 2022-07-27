dd50ae5d9df335222f92c1d8f3552bac12604684
ani <106813267+ani2689@users.noreply.github.com>
2022-07-27 19:37:20 +0900

    Update fun.c

--- fun.c
+++ fun.c
@@ -3,19 +3,28 @@
 #include <time.h>
 #include <string.h>
 #include <Windows.h>
+void Gotoxy(int x, int y)
+{
 
+    COORD pos = { x,y };
+
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+
+} //2...
 
 int SelectSummerMoive() {
 	int chiceuser1;
 	system("cls");
 	Sleep(1000);
-	gotoxy(30, 15);
+	gotoxy(30, 15); //2 (gotoxy는 함수 선 해줘야해)
 	printf("여름은 뭐니뭐니해도 공포죠?\n");
 	system("cls");
 
 	printf("여름방학 추천 영화\n");
 	printf("Real 공포 (1)\n\n");
 	printf("입문 공포 (2)\n\n");
+	
+	scanf("%d",chiceuser1); //1
 
 	return chiceuser1;
 }
