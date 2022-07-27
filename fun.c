01e10d1b4aadfc70cb2b1b086d98fdf4deaf38a2
김설희 <102404971+shk0625@users.noreply.github.com>
2022-07-27 17:51:55 +0900

    Create fun.c
    헤더 분리 안 하고.. 그냥 한 번에 때려넣은 거..

---
+++ fun.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <Windows.h>
+
+
+int SelectSummerMoive() {
+	int chiceuser1;
+	system("cls");
+	Sleep(1000);
+	gotoxy(30, 15);
+	printf("여름은 뭐니뭐니해도 공포죠?\n");
+	system("cls");
+
+	printf("여름방학 추천 영화\n");
+	printf("Real 공포 (1)\n\n");
+	printf("입문 공포 (2)\n\n");
+
+	return chiceuser1;
+}
+int SelectWinnterMoive() {
+	system("cls");
+	int chiceuser;
+
+	printf("겨울방학 추천 영화\n");
+}
+int SelcetRandom() {
+	printf("랜덤으로 영화 추천중");
+}
+int SummerMoiveHorror() {
+	printf("입문 공포 (1) ");
+}
+int SummerMoiveEnter() {
+
+}
+
+int main() {
+	int chiceuser;
+	printf("추천 경로를 숫자로 입력하세요");
+	printf("\n");
+	printf("1. 여름방학 추천 영화!\n\n");
+	printf("2. 겨울방학 추천 영화!\n\n");
+	printf("3. 선택을 못 하겠다! 랜덤! \n\n");
+	printf("\n");
+	scanf("%d", &chiceuser);
+	switch (chiceuser)
+	{
+	case 1 :
+		SelectSummerMoive();
+		break;
+	
+	case 2 :
+		SelectWinnterMoive();
+		break;
+	case 3:
+		SelectRandom();
+		break;
+
+	}
+}
+
